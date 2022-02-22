54a7c829d4fc331c01ad6164305fd3028f9eeaad
Iryusha <Sherbinina_Irina@ukr.net>
2022-02-22 23:39:02 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './sass/main.scss';
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getSearchImg } from './js/images';
+// import imagesCardTemplate from './js/components.hbs';
+
+const searchFormEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more');
+
+searchFormEl.addEventListener('submit', onSearchBtn);
+searchFormEl.addEventListener('input', onSearchInput);
+
+getSearchImg().then(({ data }) => {
+  const firstImage = data.hits[2];
+  const firstImageTamplate = imagesCardTemplate(firstImage);
+
+  galleryEl.innerHTML = firstImageTamplate;
+
+  console.log(firstImageTamplate);
+});
+
+function onSearchBtn(e) {
+  e.preventDefault();
+  console.log(searchFormEl.searchQuery.value);
+}
+
+function onSearchInput(e) {
+  console.log(e.target.value);
+}
