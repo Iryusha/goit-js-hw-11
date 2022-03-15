387c4cfc6afae8ff599b6af706e784bd1b35bbfa
Iryusha <Sherbinina_Irina@ukr.net>
2022-03-15 13:39:28 +0200

    task11

--- src/index.js
+++ src/index.js
@@ -3,29 +3,24 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { getSearchImg } from './js/API/images';
-import imagesCardTemplate from './js/components/imagesCard.hbs';
-
-
+import { getSearchImg } from './js/api/images';
+import imagesCardTemplate from './js/components/imageCard.hbs';
 
 const searchFormEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
-// const loadMoreBtnEl = document.querySelector('.load-more');
+const loadMoreBtnEl = document.querySelector('.load-more');
 
 searchFormEl.addEventListener('submit', onSearchBtn);
 searchFormEl.addEventListener('input', onSearchInput);
 
-getSearchImg().then(console.log);
+getSearchImg().then(({ data }) => {
+  const firstImage = data.hits[2];
+  const firstImageTamplate = imagesCardTemplate(firstImage);
 
-// getSearchImg().then(({ data }) => {
-  // console.log();
-  // const firstImage = data.hits[2];
-  // const firstImageTamplate = imagesCardTemplate(firstImage);
+  galleryEl.innerHTML = firstImageTamplate;
 
-  // galleryEl.innerHTML = firstImageTamplate;
-
-  // console.log(firstImageTamplate);
-// });
+  console.log(firstImageTamplate);
+});
 
 function onSearchBtn(e) {
   e.preventDefault();
@@ -35,5 +30,3 @@ function onSearchBtn(e) {
 function onSearchInput(e) {
   console.log(e.target.value);
 }
-
-
